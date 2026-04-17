28dc053d22e691cc0699b480a2276d99ebe3e1af
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-04-17 09:49:15 +0400

    up

--- packages/ui/src/styles/disciplines/carousel.ts
+++ packages/ui/src/styles/disciplines/carousel.ts
@@ -1,20 +1,17 @@
-import { tv } from "tailwind-variants";
+import { cva } from "class-variance-authority";
 
 // STYLES ----------------------------------------------------------------------------------------------------------------------------------
-const STYLES = tv({
-	base: "w-full sm:px-12",
-	slots: {
-		carousel: "flex w-full flex-col justify-center",
-		carouselControl: "hidden cursor-pointer sm:inline-flex",
-		carouselItem: "@container basis-full lg:basis-1/2 2xl:basis-1/2",
-		item: "h-full @lg:flex-row flex-col gap-6 bg-card p-6",
-		itemContent: "gap-1",
-		itemDescription: "line-clamp-none text-pretty text-center @lg:text-start font-light text-base text-foreground",
-		itemMedia: "self-center!",
-		itemTitle: "mb-3 @lg:self-start self-center font-bold font-heading text-3xl",
-		logo: "w-32",
-		mobileControl: "translate-0 static cursor-pointer",
-		mobileControls: "mt-4 flex w-32 justify-between self-center rounded-full border p-2 sm:hidden",
-	},
-});
-export const DISCIPLINES_CAROUSEL = STYLES();
+export const DISCIPLINES_CAROUSEL = {
+  base: cva("w-full sm:px-12"),
+  carousel: cva("flex w-full flex-col justify-center"),
+  carouselControl: cva("hidden cursor-pointer sm:inline-flex"),
+  carouselItem: cva("@container basis-full lg:basis-1/2 2xl:basis-1/2"),
+  item: cva("h-full @lg:flex-row flex-col gap-6 bg-card p-6"),
+  itemContent: cva("gap-1"),
+  itemDescription: cva("line-clamp-none text-pretty text-center @lg:text-start font-light text-base text-foreground"),
+  itemMedia: cva("self-center!"),
+  itemTitle: cva("mb-3 @lg:self-start self-center font-bold font-heading text-3xl"),
+  logo: cva("w-32"),
+  mobileControl: cva("translate-0! static cursor-pointer"),
+  mobileControls: cva("mt-4 flex w-32 justify-between self-center rounded-full border p-2 sm:hidden"),
+};
