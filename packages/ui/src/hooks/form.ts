bd044315ad71c85ae4834c1a31eea7825fe2b82b
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-04-23 16:48:55 +0400

    Update form.ts

--- packages/ui/src/hooks/form.ts
+++ packages/ui/src/hooks/form.ts
@@ -1,24 +1,23 @@
+import InputField from "@niama/ui/react/form/input-field";
+import SelectField from "@niama/ui/react/form/select-field";
+import Submit from "@niama/ui/react/form/submit";
+import TextareaField from "@niama/ui/react/form/textarea-field";
 import { createFormHook } from "@tanstack/react-form";
 import { lazy } from "react";
 import { fieldContext, formContext } from "./form-context";
 
-const Submit = lazy(() => import("@niama/ui/react/form/submit"));
-const InputField = lazy(() => import("@niama/ui/react/form/input-field"));
-const SelectField = lazy(() => import("@niama/ui/react/form/select-field"));
-const TextareaField = lazy(() => import("@niama/ui/react/form/textarea-field"));
-
 const DisciplinesSelectField = lazy(() => import("@niama/ui/react/disciplines/select-field"));
 
 export const { useAppForm, withForm, withFieldGroup } = createFormHook({
-	fieldComponents: {
-		DisciplinesSelectField,
-		InputField,
-		TextareaField,
-		SelectField,
-	},
-	formComponents: {
-		Submit,
-	},
-	fieldContext,
-	formContext,
+  fieldComponents: {
+    DisciplinesSelectField,
+    InputField,
+    TextareaField,
+    SelectField,
+  },
+  formComponents: {
+    Submit,
+  },
+  fieldContext,
+  formContext,
 });
