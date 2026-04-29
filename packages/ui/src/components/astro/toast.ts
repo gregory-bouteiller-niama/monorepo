69f9751cb85557ec4de67c7f4dbdc3f5782629ad
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-04-29 18:09:47 +0400

    Update toast.ts

--- packages/ui/src/components/astro/toast.ts
+++ packages/ui/src/components/astro/toast.ts
@@ -1,10 +1,10 @@
 /** biome-ignore-all lint/performance/noBarrelFile: flat Astro entrypoint exporting the component API and local styles. */
-export { TOAST, type ToastItemStyles, type ToastTitleStyles } from "./toast/styles";
 
 export { default as ToastDescription, type Props as ToastDescriptionProps } from "./toast/description.astro";
 export { default as ToastItem, type Props as ToastItemProps } from "./toast/item.astro";
+export { TOAST, type ToastItemStyles, type ToastTitleStyles } from "./toast/styles";
+export { default as Toaster, type Props as ToasterProps } from "./toast/Toaster.astro";
 export { default as ToastTemplate, type Props as ToastTemplateProps } from "./toast/template.astro";
 export { default as ToastTitle, type Props as ToastTitleProps } from "./toast/title.astro";
-export { default as Toaster, type Props as ToasterProps } from "./toast/Toaster.astro";
 export type { PromiseOptions, PromiseStateOption, ToastOptions, Variant } from "./toast/utils";
 export { toast } from "./toast/utils";
