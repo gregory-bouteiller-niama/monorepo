8896aa99b7479e9b4f4088d186122d7829d1273b
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-05-01 10:29:49 +0400

    up

--- packages/ui/src/components/react/form/submit.tsx
+++ packages/ui/src/components/react/form/submit.tsx
@@ -4,16 +4,16 @@ import { LoadingSwap } from "@niama/ui/react/loading-swap";
 import { useRef } from "react";
 
 export default function Submit() {
-	const form = useFormContext();
-	const buttonRef = useRef<HTMLButtonElement>(null);
+  const form = useFormContext();
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
-	return (
-		<form.Subscribe selector={(state) => state.isSubmitting}>
-			{(isSubmitting) => (
-				<Button className="cursor-pointer text-base" ref={buttonRef} size="lg" type="submit">
-					<LoadingSwap isLoading={isSubmitting}>Envoyer</LoadingSwap>
-				</Button>
-			)}
-		</form.Subscribe>
-	);
+  return (
+    <form.Subscribe selector={(state) => state.isSubmitting}>
+      {(isSubmitting) => (
+        <Button className="cursor-pointer text-base" ref={buttonRef} size="lg" type="submit">
+          <LoadingSwap isLoading={isSubmitting}>Envoyer</LoadingSwap>
+        </Button>
+      )}
+    </form.Subscribe>
+  );
 }
