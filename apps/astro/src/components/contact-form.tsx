4f523371cdf87058fb7c779861362b128f4b40a5
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-05-04 07:58:37 +0400

    up

---
+++ apps/astro/src/components/contact-form.tsx
@@ -0,0 +1,18 @@
+import { actions } from "astro:actions";
+import { toast } from "@niama/ui/astro/toast.ts";
+import { ContactForm as ContactFormNative } from "@niama/ui/react/contact-form";
+
+// MAIN ------------------------------------------------------------------------------------------------------------------------------------
+export function ContactForm() {
+  return (
+    <ContactFormNative
+      onSubmit={async (value) => {
+        const { error } = await actions.contact.create(value);
+        if (error) throw error;
+      }}
+      onSuccess={() => {
+        toast.success("Succès", { description: "Merci de votre intérêt ! Nous reviendrons vers vous très bientôt." });
+      }}
+    />
+  );
+}
