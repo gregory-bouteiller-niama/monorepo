3e7a9e519b34f1c38814ca0a8be12e2bab952e87
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-05-05 16:08:37 +0400

    Update the-footer.ts

--- packages/ui/src/components/shared/the-footer.ts
+++ packages/ui/src/components/shared/the-footer.ts
@@ -2,7 +2,7 @@ import { cva } from "class-variance-authority";
 
 // STYLES ----------------------------------------------------------------------------------------------------------------------------------
 export const THE_FOOTER = {
-  base: cva("container pointer-events-none fixed bottom-4 mx-auto flex w-full justify-between self-center rounded-full px-4 py-2"),
+  base: cva("pointer-events-none fixed bottom-4 mx-auto flex w-full justify-between self-center rounded-full px-4 py-2"),
   disclaimer: cva("pointer-events-auto backdrop-blur-xl"),
   social: cva("pointer-events-auto backdrop-blur-xl"),
   socials: cva("flex items-center gap-2"),
